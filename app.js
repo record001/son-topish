c8a53fcf255b6df4e1799fb7ddc68e260c8c55ca
record001 <umarkhidirboev@gmail.com>
2021-09-14 14:32:18 +0400

    main

---
+++ app.js
@@ -0,0 +1,35 @@
+
+
+let ism = prompt("Ismingizni kiriting :");
+let conf = confirm(` Assalomu alaykum ${ism} ! \n Siz 0 dan 20 gacha bo'lgan sonlarni kiritishigiz kerak va siz o'yagan son \n men o'ylagan songa teng bo'lsa g'olib bo'lasiz \n O'yinni boshlashga tayyormisiz ?`);
+
+
+
+let sonlar_toplami = [];
+let kiritilgan_sonlar;
+let oyna = document.querySelector("#son");
+let urinish = document.querySelector("#urinishlar");
+
+let random = Math.floor(Math.random() * 20 + 1);
+let a = 5;
+
+for (let i = 0; i < 5; i++) {
+  kiritilgan_sonlar = Number(prompt("O`ylagan soningizni kiriting: "));
+
+  sonlar_toplami.push(kiritilgan_sonlar);
+
+  if (random === kiritilgan_sonlar || kiritilgan_sonlar === a) {
+    alert(
+      `Siz g'olibsiz, siz ${sonlar_toplami.length} chi urunishda meni mag'lub etdingiz `
+    );
+    break;
+  } else {
+    alert("yana urinib ko`ring");
+  }
+
+  // console.log(sonlar_toplami);
+  // console.log(sonlar_toplami.length);
+}
+
+oyna.textContent = sonlar_toplami;
+urinish.textContent = sonlar_toplami.length;
